85e984c993419d1ae3537f4b997a15bfa8581848
lliuaotian <liuaotianmsj@gmail.com>
2024-05-10 14:50:30 +0800

    增加颜色设置

---
+++ lua/liuaotian/core/highlight.lua
@@ -0,0 +1,6 @@
+-- 在这个文件中完成所有窗口的颜色设置
+-- 设置cmp 自动补全窗口的颜色
+vim.api.nvim_set_hl(0, 'Pmenu', {bg=none})
+vim.api.nvim_set_hl(0, 'NormalFloat', {bg=none})
+-- 设置NvimTree 背景颜色
+vim.api.nvim_set_hl(0, 'NvimTreeNormal', {bg=none})
