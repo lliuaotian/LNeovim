e02730863e759f9771a954230343d2392fda5b32
lliuaotian <liuaotianmsj@gmail.com>
2024-05-10 11:14:39 +0800

    这个设置必须单独写一个文件，不让会冲突导致功能失效

---
+++ lua/liuaotian/plugins/cmp-cmdline.lua
@@ -0,0 +1,17 @@
+return {
+  "hrsh7th/cmp-cmdline",
+  config = function ()
+    local cmp = require("cmp")
+    cmp.setup.cmdline(":", {
+      mapping = cmp.mapping.preset.cmdline({
+        ["<CR>"] = {
+          c = cmp.mapping.confirm( {select = false } ),
+        },
+      }),
+      sources = cmp.config.sources({
+        {name = "path"},
+        {name = "cmdline"},
+      })
+    })
+  end,
+}
