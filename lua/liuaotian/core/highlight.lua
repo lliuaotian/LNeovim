122bb888c9b624da55d7c3730557b0e2e616714e
lliuaotian <liuaotianmsj@gmail.com>
2024-07-30 11:55:57 +0800

    FIX: 之前设置的NvimTree等插件的背景颜色，但后来发现没什么意义，不透明也挺好看就删掉吧

--- lua/liuaotian/core/highlight.lua
+++
@@ -1,8 +0,0 @@
--- 在这个文件中完成所有窗口的颜色设置
--- 设置cmp 自动补全窗口的颜色
-vim.api.nvim_set_hl(0, 'Pmenu', {bg=none})
-vim.api.nvim_set_hl(0, 'NormalFloat', {bg=none})
--- 设置NvimTree 背景颜色
-vim.api.nvim_set_hl(0, 'NvimTreeNormal', {bg=none})
--- 打开NvimTree背景色会显示透明，但我切换到其他文件，这个文件浏览器还显示的时候就不是透明的了
-vim.api.nvim_set_hl(0, "NvimTreeNormalNC", {bg=none})
