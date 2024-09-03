3facb2b7180da4ff1d303c1f7b71162aeda4edbc
lliuaotian <liuaotianmsj@gmail.com>
2024-09-03 18:23:18 +0800

    FEAT(cmp-ai.lua): 新增了在visual模式下选中内容可以直接翻译成中文或英文的选项

--- lua/liuaotian/plugins/cmp-ai.lua
+++ lua/liuaotian/plugins/cmp-ai.lua
@@ -2,6 +2,11 @@ return {
 
   'luozhiya/fittencode.nvim',
   config = function()
+    local keymap = vim.keymap
+    -- translate test
+    keymap.set("v", "<leader>tc", ":<C-u>Fitten translate_text_into_chinese<CR>", { silent = true, noremap = true, desc = "Translate selected text to chinese"})
+    keymap.set("v", "<leader>te", ":<C-u>Fitten translate_text_into_english<CR>", { silent = true, noremap = true, desc = "Translate selected text to english"})
+    keymap.set("v", "<leader>t", "", { desc = "Translate English or Chinese"})
     require('fittencode').setup {
       completion_mode = 'source',
     }
